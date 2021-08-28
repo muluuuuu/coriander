8d46ae7d91680b5e690d5213a71c463050516492
muluuuuu <momodarkness375gmail.com>
2021-08-28 13:59:23 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Icon(Icons.add),
+          Icon(Icons.share),
+        ],
+        title: Text('Flutter Practice'),
+      ),
+      body: Container(
+        margin: 20, 20,
+        height: 300,
+        width: 200,
+        color: Colors.red,
+        child: Padding(
+          padding: EdgeInsets.all(30),
+
+        child: Column(
+          children: <Widget>[
+            Text('いいね'),
+            Text('リツイート'),
+          ],
+        ),
+      ),
+    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
